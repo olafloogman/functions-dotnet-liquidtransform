48d47044b90d804ade0fd253c8b88964c311b0a4
Olaf Loogman <olaf@live.com.au>
2019-02-25 10:52:19 +1100

    Merging json and xml transformer

--- LiquidTransform.functionapp.v1/LiquidTransformer.cs
+++ LiquidTransform.functionapp.v1/LiquidTransformer.cs
@@ -13,10 +13,11 @@ using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Xml.Linq;
 
 namespace LiquidTransform.functionapp.v1
 {
-    public static class JsonToXml
+    public static class LiquidTransformer
     {
         /// <summary>
         /// Converts Json to XML using a Liquid mapping. The filename of the liquid map needs to be provided in the path. 
@@ -26,8 +27,8 @@ namespace LiquidTransform.functionapp.v1
         /// <param name="inputBlob"></param>
         /// <param name="log"></param>
         /// <returns></returns>
-        [FunctionName("JsonTransformer")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "jsontransformer/{liquidtransformfilename}")] HttpRequestMessage req,
+        [FunctionName("LiquidTransformer")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "liquidtransformer/{liquidtransformfilename}")] HttpRequestMessage req,
             [Blob("liquid-transforms/{liquidtransformfilename}", FileAccess.Read)] Stream inputBlob,
             TraceWriter log)
         {
@@ -41,30 +42,22 @@ namespace LiquidTransform.functionapp.v1
             // This indicates the response content type. If set to application/json it will perform additional formatting
             // Otherwise the Liquid transform is returned unprocessed.
             string responseContentType = req.Headers.Accept.FirstOrDefault().MediaType;
+            string requestContentType = req.Content.Headers.ContentType.MediaType;
 
             // Load the Liquid transform in a string
             var sr = new StreamReader(inputBlob);
             var liquidTransform = sr.ReadToEnd();
 
             string requestBody = await req.Content.ReadAsStringAsync();
-
-            // Convert the JSON input to an object tree of primitive types
-            var serializer = new JavaScriptSerializer();
-            dynamic requestJson = serializer.Deserialize(requestBody, typeof(object));
-
-            // Wrap the JSON input in another content node to provide compatibility with Logic Apps Liquid transformations
-            var transformInput = new Dictionary<string, object>();
-            transformInput.Add("content", requestJson);
-
-            var inputDictionary = Hash.FromDictionary(transformInput);
+            var inputHash = ParseRequest(requestBody, requestContentType);
 
             // Execute the Liquid transform
             Template template = Template.Parse(liquidTransform);
-            var output = template.Render(inputDictionary);
+            var output = template.Render(inputHash);
 
             if (responseContentType == "application/json")
             {
-                // This will pretty print the JSON output, and also remove redundant comma characters after arrays as a result of Liquid for loops
+                // This will pretty print the JSON output, and also remove redundant comma characters after arrays as a result of for loop constructs in Liquid
                 try
                 {
                     return new HttpResponseMessage(HttpStatusCode.OK)
@@ -74,14 +67,55 @@ namespace LiquidTransform.functionapp.v1
                 }
                 catch (System.Exception ex)
                 {
+                    // Just log the error, and return the Liquid output without JSON parsing
                     log.Error(ex.Message, ex);
                 }
 
             }
+
             return new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new StringContent(output, Encoding.UTF8, responseContentType)
             };
+        }
+
+        private static Hash ParseRequest(string requestBody, string contentType)
+        {
+            Hash inputDictionary;
+            var transformInput = new Dictionary<string, object>();
+
+            if (contentType.EndsWith("json"))
+            {
+                // Convert the JSON input to an object tree of primitive types
+                var serializer = new JavaScriptSerializer();
+                dynamic requestJson = serializer.Deserialize(requestBody, typeof(object));
+
+                // Wrap the JSON input in another content node to provide compatibility with Logic Apps Liquid transformations
+                transformInput.Add("content", requestJson);
+
+                inputDictionary = Hash.FromDictionary(transformInput);
+            }
+            else if (contentType.EndsWith("xml"))
+            {
+                var xDoc = XDocument.Parse(requestBody);
+                var json = JsonConvert.SerializeXNode(xDoc);
+
+                // Convert the XML converted JSON to an object tree of primitive types
+                var serializer = new JavaScriptSerializer();
+                dynamic requestJson = serializer.Deserialize(json, typeof(object));
+
+                // Wrap the JSON input in another content node to provide compatibility with Logic Apps Liquid transformations
+                transformInput.Add("content", requestJson);
+
+                inputDictionary = Hash.FromDictionary(transformInput);
+            }
+            else
+            {
+                // Unsupported content type
+                throw new NotSupportedException($"Media type {contentType} not supported");
+            }
+
+            return inputDictionary;
         }
     }
 }
